193104c8856af1bf20feb274dc486914c6ac4500
Arch0125 <dasarchisman25@gmail.com>
2023-12-10 01:15:59 +0530

    add generate method

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 'use client'
 import Image from 'next/image'
-import { ECDSAProvider } from "@zerodev/sdk";
-import { PrivateKeySigner } from "@alchemy/aa-core";
-import {payUsingBase} from "paasskeys-server" 
+import { payUsingBase } from "paasskeys-server"
+import { useState } from 'react';
+import { client } from '@passwordless-id/webauthn'
+import {ethers} from "ethers";
 
 export default function Home() {
 
-  let pvtKey = "";
-
+  const [privateKey, setPrivateKey] = useState("");
+  const [address, setAddress] = useState("");
+  const [ev, setEv] = useState("");
+  const[addr,setAddr]=useState("");
+  const[tx,setTx]=useState();
   const generatePopUp = async () => {
     let parentUrl = window.location.href; // Or window.location.origin for just the origin
-    let popupUrl = "http://localhost:3000?parentUrl=" + encodeURIComponent(parentUrl);
+    let popupUrl = "http://localhost:3000?parentUrl=" + encodeURIComponent(parentUrl) + "&username=" + encodeURIComponent("archis");
     let popup = window.open(popupUrl, "Generating passkey", "height=400,width=600,resizable=yes");
 
     window.addEventListener('message', async (event) => {
@@ -18,21 +22,67 @@ export default function Home() {
 
       if (typeof event.data === 'string') {
         console.log("Received value from popup:", event.data);
-        pvtKey = event.data;
+        setPrivateKey(event.data)
         popup?.close();
         //BASE Paymaster Integration
-        // try{
-        //   console.log(await payUsingBase(pvtKey))
-        // }catch(e){
-        //   console.log(e)
-        // }
+        try {
+          const { address, ev } = (await payUsingBase(privateKey))
+          setAddress(address);
+          setEv(ev);
+        } catch (e) {
+          console.log(e)
+        }
       }
     }, false);
   }
+
+  const generateClientRegister = async () => {
+    const challenge = "a7c61ef9-dc23-4806-b486-2428938a547e"
+    const registration = await client.register("archis", challenge, {
+      authenticatorType: "auto",
+      userVerification: "required",
+      timeout: 60000,
+      attestation: false,
+      userHandle: "recommended to set it to a random 64 bytes value",
+      debug: false
+    })
+
+    console.log(registration)
+    const pvtkey = ethers.keccak256(ethers.toUtf8Bytes(registration.credential.id));
+    const{address,ev}=await payUsingBase(pvtkey);
+    setAddr(address);
+    setTx(ev)
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <button onClick={generatePopUp} className='text-white'>Generate</button>
+      </div> */}
+      <div className='flex flex-row h-full w-full' >
+        <div className='flex flex-col h-full w-1/2 items-center justify-center' >
+          <p>With SDK</p>
+          <button onClick={generatePopUp} >
+            Generate
+          </button>
+          <p>
+            SCW Address : {address}
+          </p>
+          <p>
+            Transaction Hash : {ev?.transactionHash}
+          </p>
+        </div>
+        <div className='flex flex-col h-full w-1/2 items-center justify-center' >
+          <p>Without SDK</p>
+          <button onClick={generateClientRegister} >
+            Generate
+          </button>
+          <p>
+            SCW Address : {addr}
+          </p>
+          <p>
+            Transaction Hash : {tx?.transactionHash}
+          </p>
+        </div>
       </div>
     </main>
   )
