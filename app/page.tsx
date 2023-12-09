222271dcbe81c715f4f8176d5abe38cedf9aa7f8
Arch0125 <dasarchisman25@gmail.com>
2023-12-09 10:23:28 +0530

    add base paymaster

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
 'use client'
 import Image from 'next/image'
+import { ECDSAProvider } from "@zerodev/sdk";
+import { PrivateKeySigner } from "@alchemy/aa-core";
+import {payUsingBase} from "paasskeys-server" 
 
 export default function Home() {
+
+  let pvtKey = "";
 
   const generatePopUp = async () => {
     let parentUrl = window.location.href; // Or window.location.origin for just the origin
     let popupUrl = "http://localhost:3000?parentUrl=" + encodeURIComponent(parentUrl);
     let popup = window.open(popupUrl, "Generating passkey", "height=400,width=600,resizable=yes");
-        window.addEventListener('message', (event) => {
+
+    window.addEventListener('message', async (event) => {
       if (event.origin !== "http://localhost:3000") return;
 
       if (typeof event.data === 'string') {
         console.log("Received value from popup:", event.data);
+        pvtKey = event.data;
         popup?.close();
+        //BASE Paymaster Integration
+        // try{
+        //   console.log(await payUsingBase(pvtKey))
+        // }catch(e){
+        //   console.log(e)
+        // }
       }
     }, false);
   }
